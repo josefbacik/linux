c648c0d1d781e807b157b6c283a5488ffef6ad32
Josef Bacik <josef@toxicpanda.com>
2022-09-13 17:33:27 -0400

    btrfs: move BTRFS_MAX_MIRRORS into scrub.c
    This is only used locally in scrub.c, move it out of ctree.h into
    scrub.c.
    
    Reviewed-by: Qu Wenruo <wqu@suse.com>
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -50,17 +50,6 @@ struct btrfs_ref;
 struct btrfs_bio;
 struct btrfs_ioctl_encoded_io_args;
 
-/*
- * Maximum number of mirrors that can be available for all profiles counting
- * the target device of dev-replace as one. During an active device replace
- * procedure, the target device of the copy operation is a mirror for the
- * filesystem data as well that can be used to read data in order to repair
- * read errors on other disks.
- *
- * Current value is derived from RAID1C4 with 4 copies.
- */
-#define BTRFS_MAX_MIRRORS (4 + 1)
-
 #define BTRFS_OLDEST_GENERATION	0ULL
 
 #define BTRFS_EMPTY_DIR_SIZE 0
