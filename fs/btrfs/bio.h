f87a5ad8085998d751665d34b794403973c51ae9
Josef Bacik <josef@toxicpanda.com>
2023-09-22 10:24:40 -0400

    btrfs: implement process_bio cb for fscrypt
    We are going to be checksumming the encrypted data, so we have to
    implement the ->process_bio fscrypt callback.  This will provide us with
    the original bio and the encrypted bio to do work on.  For WRITE's this
    will happen after the encrypted bio has been encrypted.  For READ's this
    will happen after the read has completed and before the decryption step
    is done.
    
    For write's this is straightforward, we can just pass in the encrypted
    bio to btrfs_csum_one_bio and then the csums will be added to the bbio
    as normal.
    
    For read's this is relatively straightforward, but requires some care.
    We assume (because that's how it works currently) that the encrypted bio
    match the original bio, this is important because we save the iter of
    the bio before we submit.  If this changes in the future we'll need a
    hook to give us the bi_iter of the decryption bio before it's submitted.
    We check the csums before decryption.  If it doesn't match we simply
    error out and we let the normal path handle the repair work.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fs/btrfs/bio.h
+++ fs/btrfs/bio.h
@@ -45,6 +45,7 @@ struct btrfs_bio {
 		struct {
 			u8 *csum;
 			u8 csum_inline[BTRFS_BIO_INLINE_CSUM_SIZE];
+			bool csum_done;
 			struct bvec_iter saved_iter;
 		};
 
@@ -110,5 +111,7 @@ void btrfs_submit_repair_write(struct btrfs_bio *bbio, int mirror_num, bool dev_
 int btrfs_repair_io_failure(struct btrfs_fs_info *fs_info, u64 ino, u64 start,
 			    u64 length, u64 logical, struct page *page,
 			    unsigned int pg_offset, int mirror_num);
+blk_status_t btrfs_check_encrypted_read_bio(struct btrfs_bio *bbio,
+					    struct bio *enc_bio);
 
 #endif
