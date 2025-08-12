8af68492f3fe2b0e55914b99cf76c51c3be607fd
Josef Bacik <josef@toxicpanda.com>
2025-08-12 08:39:55 -0400

    fs: remove I_WILL_FREE|I_FREEING check from __inode_add_lru
    We only want to add to the LRU if the current caller is potentially the
    last one dropping a reference, so if our refcount is 0 we're being
    deleted, and if the refcount is > 1 then there is another ref holder and
    they can add the inode to the LRU list.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fs/inode.c
+++ fs/inode.c
@@ -649,8 +649,6 @@ static void __inode_add_lru(struct inode *inode, bool rotate)
 
 	lockdep_assert_held(&inode->i_lock);
 
-	if (inode->i_state & (I_FREEING | I_WILL_FREE))
-		return;
 	if (icount_read(inode) != 1)
 		return;
 	if (inode->__i_nlink == 0)
