77d4977e44001541edbd166396e062b03d9c16cd
Josef Bacik <josef@toxicpanda.com>
2025-08-12 10:02:36 -0400

    btrfs: don't check I_WILL_FREE|I_FREEING
    btrfs has it's own per-root inode list for snapshot uses, and it has a
    sanity check to make sure we're not overwriting a live inode when we add
    one to the root's xarray. Change this to check the refcount to validate
    it's not a live inode.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -3858,7 +3858,7 @@ static int btrfs_add_inode_to_root(struct btrfs_inode *inode, bool prealloc)
 		ASSERT(ret != -ENOMEM);
 		return ret;
 	} else if (existing) {
-		WARN_ON(!(existing->vfs_inode.i_state & (I_WILL_FREE | I_FREEING)));
+		WARN_ON(!icount_read(&existing->vfs_inode));
 	}
 
 	return 0;
