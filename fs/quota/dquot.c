51090313e8122f9c3ac45772bd8eb82c0e4b26a3
Josef Bacik <josef@toxicpanda.com>
2025-08-14 09:51:40 -0400

    fs: remove I_WILL_FREE|I_FREEING check from dquot.c
    We can use the reference count to see if the inode is live.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fs/quota/dquot.c
+++ fs/quota/dquot.c
@@ -1030,14 +1030,16 @@ static int add_dquot_ref(struct super_block *sb, int type)
 	spin_lock(&sb->s_inode_list_lock);
 	list_for_each_entry(inode, &sb->s_inodes, i_sb_list) {
 		spin_lock(&inode->i_lock);
-		if ((inode->i_state & (I_FREEING|I_WILL_FREE|I_NEW)) ||
+		if ((inode->i_state & I_NEW) ||
 		    !atomic_read(&inode->i_writecount) ||
 		    !dqinit_needed(inode, type)) {
 			spin_unlock(&inode->i_lock);
 			continue;
 		}
-		__iget(inode);
 		spin_unlock(&inode->i_lock);
+
+		if (!igrab(inode))
+			continue;
 		spin_unlock(&sb->s_inode_list_lock);
 
 #ifdef CONFIG_QUOTA_DEBUG
